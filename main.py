1de7abe5040fdf5e83c0f870408b05c3dd5cb66f
guchiko <hsnyaso@gmail.com>
2022-07-22 17:42:39 +0300

    Update main.py
    Q

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import threading
-import discord
-from flask import Flask, send_from_directory
+from flask import Flask
 
 if __name__ == '__main__':
 
@@ -10,24 +9,7 @@ if __name__ == '__main__':
     def hello_world():
         return "<p>Hello, World!</p><br><a href='mp3/勉強の欲望を持っているかどうかは能力以前の問題だ。.mp3'>asdf</a>"
 
-    @app.route('/mp3/<path:path>')
-    def send_report(path):
-        return send_from_directory('mp3s', path)
-    threading.Thread(target=lambda: app.run(host='0.0.0.0', debug=True, use_reloader=False)).start()
+    
+    app.run()
 
 
-    class MyClient(discord.Client):
-        async def on_ready(self):
-            print('Logged on as', self.user)
-
-        async def on_message(self, message):
-            # don't respond to ourselves
-            if message.author == self.user:
-                return
-
-            if message.content == 'ping':
-                await message.channel.send('pong')
-
-
-    client = MyClient()
-    # client.run('token')
