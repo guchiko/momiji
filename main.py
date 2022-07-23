f8f6c9833ad665dbd87ba5ace71d503b71b3529b
guchiko <hsnyaso@gmail.com>
2022-07-23 10:24:02 +0300

    Update main.py
    qqq

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-    def hello_world():
-        return "<p>Hello, World!</p><br><a href='mp3/勉強の欲望を持っているかどうかは能力以前の問題だ。.mp3'>asdf</a>"
+def hello_world():
+    return "<p>Hello, World!</p><br><a href='mp3/勉強の欲望を持っているかどうかは能力以前の問題だ。.mp3'>asdf</a>"
 
 if __name__ == '__main__':
     app.run()
