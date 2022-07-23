44b52082f146ad98259ec098c342410452206fb6
guchiko <hsnyaso@gmail.com>
2022-07-23 10:21:23 +0300

    qq

--- main.py
+++ main.py
@@ -2,14 +2,10 @@ import threading
 from flask import Flask
 
 app = Flask(__name__)
-if __name__ == '__main__':
 
-
-    @app.route("/")
+@app.route("/")
     def hello_world():
         return "<p>Hello, World!</p><br><a href='mp3/勉強の欲望を持っているかどうかは能力以前の問題だ。.mp3'>asdf</a>"
 
-    
+if __name__ == '__main__':
     app.run()
-
-
